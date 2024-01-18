f5a17dac163c6c7f22750a25b930e78d552211db
Dino <dsantro10@gmail.com>
2024-01-18 18:26:55 +0100

    adding web

---
+++ E2E.Employee.Web/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MatTableModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
